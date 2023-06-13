cc56c157cf70020c80f9d900153d76452935f3ee
Sanjar17 <sanjarsaidov173@gmail.com>
2023-06-13 11:18:20 +0500

    todo

--- lib/screens/groups_screen.dart
+++ lib/screens/groups_screen.dart
@@ -23,39 +23,45 @@ class _GroupsScreenState extends State<GroupsScreen> {
             child: Row(
               children: [
                 IconButton(
-                  onPressed: () {
-                    _key.currentState!.openDrawer();
-                  },
-                  icon: const Icon(Icons.menu),
-                ),
+                    onPressed: () {
+                      _key.currentState!.openDrawer();
+                    },
+                    icon: const Icon(Icons.menu)),
                 const SizedBox(width: 10),
                 const Text('Groups', style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500, color: Colors.black)),
               ],
             ),
           ),
           FutureBuilder(
-            future: Provider.of<GroupApi>(context, listen: false).getGroups(),
-            builder: (context, snapshot) => Container(
-              padding: const EdgeInsets.all(10),
-              height: MediaQuery.of(context).size.height * 0.9,
-              decoration: BoxDecoration(borderRadius: BorderRadius.circular(10)),
-              child: ListView.builder(
-                shrinkWrap: true,
-                itemCount: Provider.of<GroupApi>(context, listen: false).allGroups.length,
-                itemBuilder: (context, index) => Card(
-                  child: ListTile(
-                    title: Text(
-                      Provider.of<GroupApi>(context, listen: false).allGroups[index].name,
-                      style: const TextStyle(fontSize: 20, color: Color.fromARGB(255, 110, 109, 109)),
+              future: Provider.of<GroupApi>(context, listen: false).getGroups(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  // TODO: Change a loading screen here
+                  return const Center(child: CircularProgressIndicator());
+                } else if (snapshot.hasError) {
+                  return Center(child: Text(snapshot.error.toString()));
+                }
+                return Container(
+                  padding: const EdgeInsets.all(10),
+                  height: MediaQuery.of(context).size.height * 0.9,
+                  decoration: BoxDecoration(borderRadius: BorderRadius.circular(10)),
+                  child: ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: Provider.of<GroupApi>(context, listen: false).allGroups.length,
+                    itemBuilder: (context, index) => Card(
+                      child: ListTile(
+                        title: Text(
+                          Provider.of<GroupApi>(context, listen: false).allGroups[index].name,
+                          style: const TextStyle(fontSize: 20, color: Color.fromARGB(255, 110, 109, 109)),
+                        ),
+                        onTap: () {
+                          Navigator.pushNamed(context, 'home', arguments: Provider.of<GroupApi>(context, listen: false).allGroups[index].name.toString());
+                        },
+                      ),
                     ),
-                    onTap: () {
-                      Navigator.pushNamed(context, 'home', arguments: Provider.of<GroupApi>(context, listen: false).allGroups[index].name.toString());
-                    },
                   ),
-                ),
-              ),
-            ),
-          ),
+                );
+              }),
         ],
       ),
     );
